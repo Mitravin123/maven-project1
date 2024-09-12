415f68a25c06c1a36d911af7f542fc875e2fee47
gowthamireddy0905 <124577298+github0905@users.noreply.github.com>
2024-09-12 20:37:38 +0530

    Update App.java

--- sample-project/src/main/java/instagram/App.java
+++ sample-project/src/main/java/instagram/App.java
@@ -6,5 +6,8 @@ package instagram;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("good morning!");
+        System.out.println("Hello all!");
+        System.out.println("Hello all good night!");
     }
 }
